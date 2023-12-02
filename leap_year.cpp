7676fc331a480f9b342af373eae87e7c4b3eb07f
buzby08 <145927121+buzby08@users.noreply.github.com>
2023-12-02 18:49:58 +0000

    Update leap_year.cpp

--- leap_year.cpp
+++ leap_year.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 
 int main() {
-  int year;
   
-  std::cout << "Enter a year: ";
-  std::cin >> year;
+  int y = 0;
+  
+  std::cout << "Enter year: ";
+  std::cin >> y;
+  
+  if (y < 1000 || y > 9999) {
+    
+    std::cout << "Invalid entry.\n";
+    
+  }
+  else if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0) {
 
-
-  if (year % 4 == 0) {
-
-    if ((year % 100 == 0) and (year % 400 != 0)) {
-      std::cout << year << " is not a leap year 1\n";
-
-    } else if (year % 400 == 0) {
-      std::cout << year << " is a leap year. 2\n";
-
-    } else {
-      std::cout << year << " is not a leap year 3\n";
-
-    }
-
-  } else {
-    std::cout << year << " is not a leap year 4\n";
-
+    std::cout << y;
+    std::cout << " falls on a leap year.\n";
+    
+  }
+  else {
+    
+    std::cout << y;
+    std::cout << " is not a leap year.\n";
+    
   }
   
 }
